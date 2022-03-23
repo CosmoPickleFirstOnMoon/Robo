881b9310d7003f35a26c6cacfc426b096f162c44
Mike Murray <mikemurray056@gmail.com>
2022-03-23 10:14:17 -0400

    Equip bug fixed
    The problem was Unity was saving the object state after stopping the game. To fix this, you have to non-serialize the isEquipped variable.

--- Assets/Scripts/Skills/EmergencyHeal.cs
+++ Assets/Scripts/Skills/EmergencyHeal.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //if health goes below 1/3, the target immediately gets 50% health back, and this skill goes into cooldown.
 [CreateAssetMenu(menuName = "Skill/Emergency Heal", fileName = "skill_emergencyHeal")]
 public class EmergencyHeal : Skill
 {
-    float cooldown = 10;
     //float currentTime;
     public override void Activate(Robot target)
     {
         float lowHealth = Mathf.Round(target.maxHealth * 0.33f);
-        if (target.health < lowHealth && Time.time > target.currentTime + cooldown)
+        if (target.health < lowHealth && Time.time > currentTime + cooldown)
         {
             float healAmount = Mathf.Round(target.maxHealth * 0.5f);
 
@@ -19,7 +16,7 @@ public class EmergencyHeal : Skill
             if (target.TryGetComponent(out Player player))
             {
                 player.RestoreHealth(healAmount);
-                player.currentTime = Time.time;
+                currentTime = Time.time;
             }
             
             Debug.Log("Emergency Heal activated, " + healAmount + " TSP restored");
